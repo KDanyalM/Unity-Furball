d53d379878d3f4e0e09a9419f08782770e67fec3
Borys Gora <gora.borys@gmail.com>
2026-02-05 20:21:16 +0000

    Test scene, basic physics

---
+++ Unity-Furball/AGENTS.md
@@ -0,0 +1,8 @@
+<!-- UNITY CODE ASSIST INSTRUCTIONS START -->
+- Project name: Unity-Furball
+- Unity version: Unity 6000.3.2f1
+- Active game object:
+  - Name: Cylinder
+  - Tag: Untagged
+  - Layer: Default
+<!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Unity-Furball/Assets/Scripts/test.cs
+++ Unity-Furball/Assets/Scripts/test.cs
@@ -1,16 +1,45 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class test : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public InputAction moveAction;
+    public InputAction jumpAction;
+    Rigidbody rigidbody;
+    Vector3 move;
+    public static float jumpForce = 200;
+    public float moveSpeed = 20.0f;
+    Vector3 jump = new Vector3(0, jumpForce, 0);
+
     void Start()
     {
-        
+        moveAction.Enable();
+        jumpAction.Enable();
+        rigidbody = GetComponent<Rigidbody>();     
     }
 
-    // Update is called once per frame
     void Update()
     {
+        move = moveAction.ReadValue<Vector3>();
+
+        if (jumpAction.WasPerformedThisFrame())
+        {
+            Jump();
+        }
         
+
+
+    }
+    void Jump()
+    {
+        rigidbody.AddForce(jump);
+    }
+
+
+    private void FixedUpdate()
+    {
+        Vector3 position = (Vector3)rigidbody.position + move * moveSpeed * Time.deltaTime;
+        rigidbody.MovePosition(position);
+
     }
 }
