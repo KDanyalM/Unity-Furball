e5b98869188222aed23af1d0b53c5ea44386389d
MaggyHanma <kamranmuir41@gmail.com>
2026-02-14 10:15:48 +0000

    Lots of Inventory stuff
    Added visual for inventory, slot system and other

--- Unity-Furball/Assets/Scripts/Inventory System/InventoryManager.cs
+++ Unity-Furball/Assets/Scripts/Inventory System/InventoryManager.cs
@@ -1,16 +1,61 @@
+using NUnit.Framework;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class InventoryManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private GameObject slotHolder;
+    [SerializeField] private ItemClass itemToAdd;
+    [SerializeField] private ItemClass itemToRemove;
+    
+    
+
+    public List<ItemClass> items = new List<ItemClass>();
+
+    private GameObject[] slots;
+
+    public void Start()
     {
+        slots = new GameObject[slotHolder.transform.childCount];
+        //sets all the slots
+        for (int i = 0; i < slotHolder.transform.childCount; i++)
+        
+            slots[i] = slotHolder.transform.GetChild(i).gameObject;
+
+        RefreshUI();
+
+        Add(itemToAdd);
         
     }
-
-    // Update is called once per frame
-    void Update()
+    // Refreshes the UI to place images where item data is held.
+    public void RefreshUI()
     {
-        
+        for (int i = 0; i < slots.Length; i++)
+        {
+            try
+            {
+                slots[i].transform.GetChild(0).GetComponent<Image>().enabled = true;
+                slots[i].transform.GetChild(0).GetComponent<Image>().sprite = items[i].itemIcon;
+            }
+            // Ensures there is no crash and checks if there is no item, then there is no sprite.
+            catch
+            {
+                slots[i].transform.GetChild(0).GetComponent<Image>().sprite = null;
+                slots[i].transform.GetChild(0).GetComponent<Image>().enabled = false;
+            }
+        }
+    }
+
+    public void Add(ItemClass item)
+    {
+        items.Add(item);
+        RefreshUI();
+    }
+
+    public void Remove(ItemClass item)
+    {
+        RefreshUI();
     }
 }
